c5a09f489017fa14936fa8e2f6eeb00b442a3dc1
linyekaye <37256323+linyekaye@users.noreply.github.com>
2018-11-05 00:03:28 -0500

    Dicked around with various methods and fields.

--- 2A-sync/src/main/java/cs131/pa2/Abstract/Vehicle.java
+++ 2A-sync/src/main/java/cs131/pa2/Abstract/Vehicle.java
@@ -1,8 +1,11 @@
 package cs131.pa2.Abstract;
 import java.util.*;
+import java.time.Duration;
+import java.time.Instant;
 
 import cs131.pa2.Abstract.Log.EventType;
 import cs131.pa2.Abstract.Log.Log;
+import cs131.pa2.CarsTunnels.Ambulance;
 
 /**
  * A Vehicle is a Runnable which enters tunnels. You must subclass
@@ -21,188 +24,225 @@ import cs131.pa2.Abstract.Log.Log;
  * integer that it returns is used as the speed for the vehicle.
  */
 public abstract class Vehicle implements Runnable {
-    private String            	name;
-    private Direction          	direction;
-    private Collection<Tunnel> 	tunnels;
-    private int                	priority;
-    private int                	speed;
-    private Log 				log;
+	private String            	name;
+	private Direction          	direction;
+	private Collection<Tunnel> 	tunnels;
+	private int                	priority;
+	private int                	speed;
+	private Log 				log;
+	private Instant				startTime; // when the vehicle enters the tunnel
+	private Instant				endTime; // when the vehicle pulls over
+	private int					requiredTime; // milliseconds a vehicle will need to pass through the tunnel
 
-    /**
-     * Initialize a Vehicle; called from Vehicle constructors.
-     */
-    private void init(String name, Direction direction,
-                      int priority, Log log) {
-        this.name      = name;
-        this.direction = direction;
-        this.priority  = 0;
-        this.speed     = getDefaultSpeed();
-        this.log       = log;
-        this.tunnels   = new ArrayList<Tunnel>();
+	/**
+	 * Initialize a Vehicle; called from Vehicle constructors.
+	 */
+	private void init(String name, Direction direction,
+			int priority, Log log) {
+		this.name      = name;
+		this.direction = direction;
+		this.priority  = 0;
+		this.speed     = getDefaultSpeed();
+		this.log       = log;
+		this.tunnels   = new ArrayList<Tunnel>();
 
-        if(this.speed < 0 || this.speed > 9) {
-            throw new RuntimeException("Vehicle has invalid speed");
-        }
-    }
+		if(this.speed < 0 || this.speed > 9) {
+			throw new RuntimeException("Vehicle has invalid speed");
+		}
+	}
 
-    /**
-     * Override in a subclass to determine the speed of the
-     * vehicle.
-     *
-     * Must return a number between 0 and 9 (inclusive). Higher
-     * numbers indicate greater speed. The faster a vehicle, the less
-     * time it will spend in a tunnel.
-     */
-    protected abstract int getDefaultSpeed();
+	/**
+	 * Override in a subclass to determine the speed of the
+	 * vehicle.
+	 *
+	 * Must return a number between 0 and 9 (inclusive). Higher
+	 * numbers indicate greater speed. The faster a vehicle, the less
+	 * time it will spend in a tunnel.
+	 */
+	protected abstract int getDefaultSpeed();
 
-    /**
-     * Create a Vehicle with default priority that can cross one of
-     * a collection of tunnels.
-     * 
-     * @param name      The name of this vehicle to be displayed in the
-     *                  output.
-     * @param direction The side of the tunnel being entered.
-     */
-    public Vehicle(String name, Direction direction, Log log) {
-        init(name, direction, 0, log);
-    }
-    
-    public Vehicle(String name, Direction direction) {
-        this(name, direction, Tunnel.DEFAULT_LOG);
-    }
-    
-    /**
-     * Sets this vehicle's speed - used for preemptive priority scheduler test
-     * @param speed the new speed to be set (0 to 9)
-     */
-    public void setSpeed(int speed) {
-    	if(this.speed < 0 || this.speed > 9) {
-            throw new RuntimeException("Invalid speed: "+ speed);
-        }
-    	this.speed = speed;
-    }
-    
-    /** 
-     * Sets this vehicle's priority - used for priority scheduling
-     *
-     * @param priority The new priority (between 0 and 4 inclusive)
-     */
-    public final void setPriority(int priority) {
-        if(priority < 0 || priority > 4) {
-            throw new RuntimeException("Invalid priority: " + priority);
-        }
-        this.priority = priority;
-    }
-    
-    /**
-     * Returns the priority of this vehicle
-     *
-     * @return This vehicle's priority.
-     */
-    public int getPriority() {
-        return priority;
-    }
+	/**
+	 * Create a Vehicle with default priority that can cross one of
+	 * a collection of tunnels.
+	 * 
+	 * @param name      The name of this vehicle to be displayed in the
+	 *                  output.
+	 * @param direction The side of the tunnel being entered.
+	 */
+	public Vehicle(String name, Direction direction, Log log) {
+		init(name, direction, 0, log);
+	}
 
-    /**
-     * Returns the name of this vehicle
-     *
-     * @return The name of this vehicle
-     */
-    public final String getName() {
-        return name;
-    }
+	public Vehicle(String name, Direction direction) {
+		this(name, direction, Tunnel.DEFAULT_LOG);
+	}
 
-    public String toString() {
-    		return String.format("%s VEHICLE %s", this.direction, this.name);
-    }
-    
-    public final void addTunnel(Tunnel newTunnel) {
-        this.tunnels.add(newTunnel);
-    }
+	/**
+	 * Sets this vehicle's speed - used for preemptive priority scheduler test
+	 * @param speed the new speed to be set (0 to 9)
+	 */
+	public void setSpeed(int speed) {
+		if(this.speed < 0 || this.speed > 9) {
+			throw new RuntimeException("Invalid speed: "+ speed);
+		}
+		this.speed = speed;
+	}
 
-    public final void addTunnel(Collection<Tunnel> newTunnels) {
-        this.tunnels.addAll(newTunnels);
-    }
-    
-    /**
-     * Find and cross through one of the tunnels.
-     * 
-     * When a thread is run, it keeps looping through its collection
-     * of available tunnels until it succeeds in entering one of
-     * them. Then, it will call doWhileInTunnel (to simulate doing
-     * some work inside the tunnel, i.e., that it takes time to cross
-     * the tunnel), then exit that tunnel.
-     */
-    public final void run() {
-        // Loop over all tunnels repeated until we can enter one, then
-        // think inside the tunnel, exit the tunnel, and leave this
-        // entire method.
-        //
-        while(true) {
-            for(Tunnel tunnel : tunnels) {
-                if(tunnel.tryToEnter(this)) {
-                    doWhileInTunnel();
-                    tunnel.exitTunnel(this);
-                    this.log.addToLog(this, EventType.COMPLETE);
-                    return; // done, so leave the whole function
-                }
-            }
-        }
-    }
-    
-    /**
-     * Returns the direction of this vehicle
-     *
-     * @return the direction of this vehicle
-     */
-    public final Direction getDirection() {
-        return direction;
-    }
+	/** 
+	 * Sets this vehicle's priority - used for priority scheduling
+	 *
+	 * @param priority The new priority (between 0 and 4 inclusive)
+	 */
+	public final void setPriority(int priority) {
+		if(priority < 0 || priority > 4) {
+			throw new RuntimeException("Invalid priority: " + priority);
+		}
+		this.priority = priority;
+	}
 
-    /**
-     * This is what your vehicle does while inside the tunnel to
-     * simulate taking time to "cross" the tunnel. The faster your
-     * vehicle is, the less time this will take.
-     */
-    public final void doWhileInTunnel() {
-         try {
-             Thread.sleep((10 - speed) * 100);
-         } catch(InterruptedException e) {
-             System.err.println("Interrupted vehicle " + getName());
-         }
-    }
-    
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 23 * hash + Objects.hashCode(this.name);
-        hash = 23 * hash + Objects.hashCode(this.direction);
-        hash = 23 * hash + this.speed;
-        hash = 23 * hash + this.priority;
-        return hash;
-    }
+	/**
+	 * Returns the priority of this vehicle
+	 *
+	 * @return This vehicle's priority.
+	 */
+	public int getPriority() {
+		return priority;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Vehicle other = (Vehicle) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        if (this.direction != other.direction) {
-            return false;
-        }
-        if (this.speed != other.speed) {
-            return false;
-        }
-        if (this.priority != other.priority) {
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * Returns the name of this vehicle
+	 *
+	 * @return The name of this vehicle
+	 */
+	public final String getName() {
+		return name;
+	}
+
+	public String toString() {
+		return String.format("%s VEHICLE %s", this.direction, this.name);
+	}
+
+	public final void addTunnel(Tunnel newTunnel) {
+		this.tunnels.add(newTunnel);
+	}
+
+	public final void addTunnel(Collection<Tunnel> newTunnels) {
+		this.tunnels.addAll(newTunnels);
+	}
+
+	/**
+	 * Find and cross through one of the tunnels.
+	 * 
+	 * When a thread is run, it keeps looping through its collection
+	 * of available tunnels until it succeeds in entering one of
+	 * them. Then, it will call doWhileInTunnel (to simulate doing
+	 * some work inside the tunnel, i.e., that it takes time to cross
+	 * the tunnel), then exit that tunnel.
+	 */
+	public final void run() {
+		// Loop over all tunnels repeated until we can enter one, then
+		// think inside the tunnel, exit the tunnel, and leave this
+		// entire method.
+		while(true) {
+			for (Tunnel tunnel : tunnels) {
+				if(tunnel.tryToEnter(this)) {
+					startTime = Instant.now();
+					doWhileInTunnel();
+					tunnel.exitTunnel(this);
+					if (this instanceof Ambulance) {
+						tunnel.notifyAll();
+					}
+					this.log.addToLog(this, EventType.COMPLETE);
+					return; // done, so leave the whole function
+				}
+			}
+		}
+	}
+
+	/**
+	 * Returns the direction of this vehicle
+	 *
+	 * @return the direction of this vehicle
+	 */
+	public final Direction getDirection() {
+		return direction;
+	}
+
+	/**
+	 * This is what your vehicle does while inside the tunnel to
+	 * simulate taking time to "cross" the tunnel. The faster your
+	 * vehicle is, the less time this will take.
+	 */
+	public final void doWhileInTunnel() {
+		try {
+			requiredTime = ((10 - speed) * 100);
+			Thread.sleep(requiredTime);
+		} catch(InterruptedException e) {
+			System.err.println("Interrupted vehicle " + getName());
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 23 * hash + Objects.hashCode(this.name);
+		hash = 23 * hash + Objects.hashCode(this.direction);
+		hash = 23 * hash + this.speed;
+		hash = 23 * hash + this.priority;
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Vehicle other = (Vehicle) obj;
+		if (!Objects.equals(this.name, other.name)) {
+			return false;
+		}
+		if (this.direction != other.direction) {
+			return false;
+		}
+		if (this.speed != other.speed) {
+			return false;
+		}
+		if (this.priority != other.priority) {
+			return false;
+		}
+		return true;
+	}
+	
+	public void setStartTime() {
+		startTime = Instant.now();
+	}
+	
+	public void setEndTime() {
+		endTime = Instant.now();
+	}
+	
+	public void pullOver() {
+		try {
+			this.wait();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		setEndTime();
+	}
+	
+	public void restart() {
+		requiredTime = requiredTime - (int)Duration.between(startTime, endTime).toMillis(); // account for time the 
+		this.notify();
+		setStartTime();
+		try {
+			this.wait(requiredTime);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
